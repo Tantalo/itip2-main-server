fad758e28fa0cfaf58ecdaae99d6945b3fb4502b
Tantalo <christianalessandro.tantalo@gmail.com>
2021-05-11 11:03:11 +0200

    getUserDb

---
+++ itip2-db.sql
@@ -0,0 +1,2 @@
+CREATE TABLE `itip2`.`MacAddressUserDB` ( `MacAddress` VARCHAR(20) NOT NULL , `UserDB` VARCHAR(20) NOT NULL ) ENGINE = InnoDB;
+
--- routerServer.js
+++ routerServer.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const db_host = 'localhost';
+const db_user = 'me';
+const db_pwd = '';
+const database = ''
+
 const express = require('express');
 const routerServer = express.Router();
 var cors = require('cors');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+
+var macAddressUserDB = {};
 
 
     routerServer.use(cors());
@@ -13,7 +21,7 @@ var bodyParser = require('body-parser')
     }));
 
     routerServer.get('/', (req,res)=>{
-        res.send({message: 'Hello World Main Server!'});
+        res.send({message: 'Hello World from Main Server!'});
     });
 
 
@@ -21,11 +29,47 @@ var bodyParser = require('body-parser')
         var ssid = req.body.ssid;
         var pwd = req.body.pwd;
         console.log('pwd: ' + pwd);
-
-
-
     });
 
+    routerServer.post('/getUserDb', (req,res) => {
+        var macAddress = req.body.macAddress;
+        console.log('macAddress: ' + macAddress);
+        let userDb = getUserDb(macAddress);
+        res.send(userDb);
+    });
+
+    function getUserDb(macAddress) {
+        if (macAddressUserDB[macAddress] && macAddressUserDB[macAddress].trim() !== "") {
+            return macAddressUserDB[macAddress];
+        }
+
+        let rtn = null;
+        var connection = getConnection();
+        connection.connect();
+
+        connection.query('SELECT UserDb from itip2.MacAddressUserDB where MacAddress = ?', macAddress, function (error, results, fields) {
+            if (error) {
+                console.log('Error selecting user db', error);
+            } else {
+                rtn = results[0].UserDb;
+                macAddressUserDB[macAddress] = rtn;
+            }
+          });
+
+        connection.end();
+
+        return rtn;
+    }
+
+    function getConnection() {
+        var connection = mysql.createConnection({
+            host     : db_host,
+            user     : db_user,
+            password : db_pwd,
+            database : database
+        });
+        return connection;
+    }
 
 
 module.exports = routerServer;
